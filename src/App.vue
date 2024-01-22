f6b80eddd8947039c46f72e4d53638c7872f2582
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2024-01-22 21:21:15 +0100

    ex completed

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <script>
+export default {
+  name: "App",
+  data() {
+    return {
+      message: "Salvatore",
+      content: "lorem ipsum dolor",
+      msg: "lorem",
+      name: " ",
+      lastname: " ",
+    }
+  },
+  methods: {
+    testFunction() {
+      console.log('This is function');
+    }
+  }
+};
 </script>
 
 <template>
   <header>
-    <h1>Hello World!</h1>
+    <h1>Hello {{ message }}</h1>
+    <p>{{ content }}</p>
+    <input type="text" v-model="msg">
+    <div>
+      <label for="name">First name</label>
+      <input type="text" v-model="name">
+      <label for="lastname">Last name</label>
+      <input type="text" v-model="lastname">
+      <p>Ciao {{ name }} {{ lastname }}</p>
+    </div>
+
+    <button @click="testFunction">Click me</button>
   </header>
 </template>
 
